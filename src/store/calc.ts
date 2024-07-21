9b70979547d7534c6b74ae3244e9092af05ff463
toaonly <toaonly42@gmail.com>
2024-07-21 22:36:19 +0900

    refactor: 코드 리팩토링 및 설정 변경
    - VSCode 설정에서 'source.fixAll' 옵션을 'explicit'로 변경
    - calc.ts 파일에서 계산 로직 수정
      - lodash의 round 함수를 사용하여 반올림 처리 추가
      - 이미 낸 금액, 내야할 금액, 최종 지불 금액을 계산하는 로직을 computed 속성으로 변경
      - members.list와 payments.list가 변화할 때마다 resultMap이 업데이트되도록 watch 함수 추가

--- src/store/calc.ts
+++ src/store/calc.ts
@@ -1,38 +1,15 @@
+import { round } from 'lodash-es'
 import { useMembersStore, usePaymentsStore } from './'
-import type { Member } from './'
 
-const toFixed = (value: number, fracDigit: number) => +value.toFixed(fracDigit)
+import type { Member } from './'
+import { computed, watch } from 'vue'
 
 export const useCalc = () => {
   const members = useMembersStore()
   const payments = usePaymentsStore()
-  const getResultMap = () =>
-    members.list.reduce(
-      (acc, mem) => {
-        const paymentsByMem = payments.findsByMemberId(mem.id)
-        const already = paymentsByMem.reduce(
-          (result, cur) => result + cur.amount,
-          0
-        )
-        const toBePaid = payments.list
-          .filter(p => p.members.find(memId => memId === mem.id))
-          .reduce(
-            (result, cur) =>
-              result + toFixed(cur.amount / cur.members.length, 0),
-            0
-          )
-        const result = toBePaid - already
-
-        return {
-          ...acc,
-          [mem.id]: {
-            already,
-            toBePaid,
-            result,
-          },
-        }
-      },
-      {} as Record<
+  const resultMap = computed(() => {
+    return members.list.reduce<
+      Record<
         Member['id'],
         {
           already: number // 이미 낸 금액
@@ -40,7 +17,69 @@ export const useCalc = () => {
           result: number // 내야할 금액 - 이미 낸 금액 = 최종 지불 금액
         }
       >
-    )
+    >((acc, mem) => {
+      const paymentsByMem = payments.findsByMemberId(mem.id)
+      const already = paymentsByMem.reduce(
+        (result, cur) => result + cur.amount,
+        0
+      )
+      const toBePaid = payments.list
+        .filter(p => p.members.find(memId => memId === mem.id))
+        .reduce(
+          (result, cur) => result + round(cur.amount / cur.members.length, 0),
+          0
+        )
+      const result = toBePaid - already
+
+      return {
+        ...acc,
+        [mem.id]: {
+          already,
+          toBePaid,
+          result,
+        },
+      }
+    }, {})
+  })
+  watch(
+    () => [members.list, payments.list],
+    () => {
+      console.log(resultMap)
+    }
+  )
+  const getResultMap = () =>
+    members.list.reduce<
+      Record<
+        Member['id'],
+        {
+          already: number // 이미 낸 금액
+          toBePaid: number // 내야할 금액
+          result: number // 내야할 금액 - 이미 낸 금액 = 최종 지불 금액
+        }
+      >
+    >((acc, mem) => {
+      const paymentsByMem = payments.findsByMemberId(mem.id)
+      const already = paymentsByMem.reduce(
+        (result, cur) => result + cur.amount,
+        0
+      )
+      const toBePaid = payments.list
+        .filter(p => p.members.find(memId => memId === mem.id))
+        .reduce(
+          (result, cur) => result + round(cur.amount / cur.members.length, 0),
+          0
+        )
+      const result = toBePaid - already
+
+      return {
+        ...acc,
+        [mem.id]: {
+          already,
+          toBePaid,
+          result,
+        },
+      }
+    }, {})
 
   return { getResultMap }
 }
